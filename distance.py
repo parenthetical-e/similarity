a689f7766122124e3ee5a66a490b70f25fae4413
Erik Peterson <Erik.Exists@gmail.com>
2012-06-16 17:15:13 -0600

    Init.
    Initial go at similarity functions for the catreward project.

---
+++ distance.py
@@ -0,0 +1,12 @@
+""" Distance measures. """
+
+def l2(x1, x2, axis=0):
+    """
+    Returns the 2d euclidian distance (L2) between x1 and x2.
+    """
+
+    x1 = np.array(x1)
+    x2 = np.array(x2)
+    distances = np.sqrt(x1 ** 2 + x2 ** 2)
+
+    return distances
